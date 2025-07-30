b8e4121eadf34cac49504ff3092ec6fb102f01df
VIKRAM <kumar03.rkvalley@gmail.com>
2025-07-30 21:43:43 +0530

    This is updated problem

---
+++ count_longest_subArray.java
@@ -0,0 +1,33 @@
+/*Count subarrays with given xor K
+
+
+0
+
+100
+Hard
+
+Hints
+Given an array of integers nums and an integer k, return the total number of subarrays whose XOR equals to k.
+
+
+Examples:
+Input : nums = [4, 2, 2, 6, 4], k = 6
+
+
+
+
+Output : 4
+
+
+
+Explanation : The subarrays having XOR of their elements as 6 are [4, 2],  [4, 2, 2, 6, 4], [2, 2, 6], and [6]
+
+Input :nums = [5, 6, 7, 8, 9], k = 5
+
+
+
+Output : 2
+
+*/
+
+
